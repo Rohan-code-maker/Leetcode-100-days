6fc869debe9f73e9b356e512d8cb543f3e1c3dd5
Rohan Kumar Gupta <rohan.gupta2059@gmail.com>
2025-08-05 22:14:13 +0530

    Day5: 1518)Water Bottles problem

---
+++ WaterBottles.java
@@ -0,0 +1,20 @@
+class WaterBottles {
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        if (numBottles < numExchange) {
+            return numBottles;
+        } else {
+            int sum = numBottles;
+            while (numBottles >= numExchange) {
+                int newBottles = numBottles / numExchange;
+                int remainingBottles = numBottles % numExchange;
+                sum += newBottles;
+                numBottles = newBottles + remainingBottles;
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numWaterBottles(9, 3));
+    }
+}
