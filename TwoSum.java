6b2510fefc4cbd89e73fe1e71b0a5ddf8477b37b
Rohan Kumar Gupta <rohan.gupta2059@gmail.com>
2025-08-06 08:43:07 +0530

    Day6: 1) Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,21 @@
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1; j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int target = 7;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
