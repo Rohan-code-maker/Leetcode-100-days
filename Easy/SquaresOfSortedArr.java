fb902c70780de917d5b6687b47504658095e43da
Rohan Kumar Gupta <rohan.gupta2059@gmail.com>
2025-08-10 14:20:23 +0530

    Day12: 977) Squares of sorted array

---
+++ Easy/SquaresOfSortedArr.java
@@ -0,0 +1,32 @@
+package Easy;
+
+public class SquaresOfSortedArr {
+
+    public static int[] sortedSquares(int[] nums) {
+        int[] ans = new int[nums.length];
+        int ptr = ans.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int ss = nums[start] * nums[start];
+            int es = nums[end] * nums[end];
+            if (ss >= es) {
+                ans[ptr] = ss;
+                start++;
+            } else {
+                ans[ptr] = es;
+                end--;
+            }
+            ptr--;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -7, -4, 0, 1, 2 };
+        int[] result = sortedSquares(nums);
+        for (int val : result) {
+            System.out.println(val);
+        }
+    }
+}
